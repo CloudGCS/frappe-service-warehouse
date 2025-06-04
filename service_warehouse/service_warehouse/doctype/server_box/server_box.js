07c13d6f39fb9d2f177f1b24bd56d26f6b19f03b
Engin Can Altınöz <altinozengincan@gmail.com>
2025-06-04 22:22:30 +0300

    new docTypes are added.

---
+++ service_warehouse/service_warehouse/doctype/server_box/server_box.js
@@ -0,0 +1,40 @@
+// Copyright (c) 2025, CloudGCS and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on("Server Box", {
+	refresh(frm) {
+		frm.add_custom_button("Get Installation Zip", function () {
+			get_zip_content(frm, "get_installation_zip");
+		});
+		frm.add_custom_button("Get Update Zip", function () {
+			get_zip_content(frm, "get_update_zip");
+		});
+	},
+});
+
+function get_zip_content(frm, command) {
+	frappe.call({
+		method: `service_warehouse.service_warehouse.doctype.server_box.server_box.${command}`,
+		args: { server_box_id: frm.doc.name },
+		callback: (r) => {
+			if (r.message) {
+				const base64Data = r.message.content_base64;
+				const byteCharacters = atob(base64Data);
+				const byteNumbers = new Array(byteCharacters.length);
+				for (let i = 0; i < byteCharacters.length; i++) {
+					byteNumbers[i] = byteCharacters.charCodeAt(i);
+				}
+				const byteArray = new Uint8Array(byteNumbers);
+				const blob = new Blob([byteArray], { type: "application/zip" });
+				// Create a link and trigger download
+				const link = document.createElement("a");
+				link.href = window.URL.createObjectURL(blob);
+				link.download = `${r.message.filename}`;
+				document.body.appendChild(link);
+				link.click();
+				document.body.removeChild(link);
+				frm.reload_doc();
+			}
+		},
+	});
+}
---
+++ service_warehouse/service_warehouse/doctype/server_box_version/server_box_version.py
@@ -0,0 +1,36 @@
+# Copyright (c) 2025, CloudGCS and contributors
+# For license information, please see license.txt
+
+import frappe
+from frappe.model.document import Document
+
+
+class ServerBoxVersion(Document):
+    # begin: auto-generated types
+    # This code is auto-generated. Do not modify anything in this block.
+
+    from typing import TYPE_CHECKING
+
+    if TYPE_CHECKING:
+        from frappe.types import DF
+
+        install_zip: DF.Attach
+        name: DF.Int | None
+        update_zip: DF.Attach
+        version_name: DF.Data
+    # end: auto-generated types
+    pass
+
+    def validate(self):
+        user = frappe.session.user
+        user_data = frappe.get_doc("User", user).as_dict()
+        tenant = frappe.get_all(
+            "Tenant",
+            filters={"user": user_data.get("email")},
+            fields=["name"],
+        )
+        if not tenant or len(tenant) == 0:
+            frappe.throw("Tenant not found for the user.")
+        tenant_name = tenant[0].name
+        if tenant_name != "HOST":
+            frappe.throw("Only SYSTEM user can create Server Box Version.")
