9ae26a943d47114182727a60f5e4dd8e4d1b5c0d
Engin Can Altınöz <altinozengincan@gmail.com>
2025-08-18 10:14:46 +0300

    bugfix for getting update zip

--- service_warehouse/service_warehouse/doctype/server_box/server_box.py
+++ service_warehouse/service_warehouse/doctype/server_box/server_box.py
@@ -33,10 +33,7 @@ class ServerBox(Document):
         if self.box_type == "Service Box":
             tenant_client_box = frappe.get_all(
                 "Server Box",
-                filters={
-                    "box_type": "Client Box",
-                    "tenant": self.tenant
-                },
+                filters={"box_type": "Client Box", "tenant": self.tenant},
                 fields=["name"],
             )
             if len(tenant_client_box) == 0:
@@ -53,13 +50,30 @@ class ServerBox(Document):
         box_type = self.box_type.replace(" Box", "")
         instance_name = f"{self.tenant}-{box_type}-{self.name}"
         instance_name = instance_name.lower()
-        frappe.db.set_value(self.doctype, self.name, "owner", tenant.user, update_modified=False)
-        frappe.db.set_value(self.doctype, self.name, "instance_name", instance_name, update_modified=False)
+        frappe.db.set_value(
+            self.doctype, self.name, "owner", tenant.user, update_modified=False
+        )
+        frappe.db.set_value(
+            self.doctype,
+            self.name,
+            "instance_name",
+            instance_name,
+            update_modified=False,
+        )
         frappe.db.commit()
         self.reload()
 
 
 def get_zip_file_content(server_box_id, field_name):
+    def get_next_id(current_id):
+        result = frappe.db.get_value(
+            "Server Box Version",
+            filters={"name": [">", current_id]},
+            fieldname="name",
+            order_by="name ASC",
+        )
+        return result  # Returns the next higher ID or None if not found
+
     if not server_box_id:
         frappe.throw("Server Box ID is required to get the zip file.")
     server_box = frappe.get_doc("Server Box", server_box_id)
@@ -67,9 +81,19 @@ def get_zip_file_content(server_box_id, field_name):
         frappe.throw(f"Server Box with ID {server_box_id} does not exist.")
     if not server_box.server_box_version:
         frappe.throw("Server Box Version is not set for this Server Box.")
-    server_box_version = frappe.get_doc(
-        "Server Box Version", server_box.server_box_version
-    )
+    if field_name == "install_zip":
+        server_box_version = frappe.get_doc(
+            "Server Box Version", server_box.server_box_version
+        )
+    elif field_name == "update_zip":
+        next_server_box_version_id = get_next_id(server_box.server_box_version)
+        if not next_server_box_version_id:
+            frappe.msgprint("You are using latest Server Box Version. No Update!")
+            return None
+        server_box_version = frappe.get_doc(
+            "Server Box Version", next_server_box_version_id
+        )
+
     file_url = getattr(server_box_version, field_name, None)
     if not file_url:
         frappe.throw(
@@ -79,7 +103,6 @@ def get_zip_file_content(server_box_id, field_name):
     with open(file_doc.get_full_path(), "rb") as f:
         encoded = base64.b64encode(f.read()).decode()
     return {"filename": file_doc.file_name, "content_base64": encoded, "success": True}
-
 
 
 @frappe.whitelist()
@@ -99,12 +122,16 @@ def get_update_zip(*args, **kwargs):
     except Exception as e:
         frappe.throw(f"Error while getting update zip: {str(e)}")
 
+
 @frappe.whitelist()
 def custom_link_query(doctype, txt, searchfield, start, page_len, filters):
-    return frappe.db.sql("""
+    return frappe.db.sql(
+        """
         SELECT `name`, `version_name`
         FROM `tabServer Box Version`
         WHERE `version_name` LIKE %s
         ORDER BY `version_name`
         LIMIT %s OFFSET %s
-    """, ("%%%s%%" % txt, page_len, start))
+    """,
+        ("%%%s%%" % txt, page_len, start),
+    )
